7d14990190cbac8baac57f6b945759d62a4a4569
Fillipe <fillipe@querycommerce.com>
2022-05-18 19:51:00 -0300

    Alteracoes no codigo para atender as requisicoes de acordo com o enunciado do trabalho

--- server.js
+++ server.js
@@ -3,13 +3,63 @@ const express = require("express")
 let app = express()
 
 app.get('/', function(req,res) {
-    return res.send("recebi requisição GET")
+    return returnSuccessMessage(req, res)
+})
+
+app.get('/produtos', function(req,res) {
+    return returnSuccessMessage(req, res)
+})
+
+app.get('/filmes/:id?', function(req,res) {
+    if (!req.params.id) {
+        return returnErrorMessage(req, res)
+    }
+
+    return returnSuccessMessage(req, res)
+})
+
+app.post('/musica/:id?/:autor?/:genero?', function(req,res) {
+    if (!req.params.id || !req.params.autor || !req.params.genero) {
+        return returnErrorMessage(req, res)
+    }
+
+    return res.json({
+        id     : req.params.id,
+        autor  : req.params.autor,
+        genero : req.params.genero
+    })
 })
 
 app.post('/', function(req,res) {
-    return res.send("recebi requisição POST")
+    return returnSuccessMessage(req, res)
 })
 
 app.listen(3000, function() {
     console.log("Servidor rodando na porta 3000 ...");
 })
+
+/**
+ * Retorna codigo 400 para o status da requisicao HTTP e escreve a
+ * mensagem de parametros invalidos
+ * @param IncomingMessage req 
+ * @param ServerResponse res 
+ * @returns ServerResponse
+ */
+function returnErrorMessage(req, res)
+{
+    res.status(400)
+    return res.send('parametros inválidos')
+}
+
+/**
+ * Retorna codigo 200 para o status da requisicao HTTP e escreve a
+ * mensagem com a rota acessada
+ * @param IncomingMessage req 
+ * @param ServerResponse res 
+ * @returns ServerResponse
+ */
+function returnSuccessMessage(req, res)
+{
+    res.status(200)
+    return res.send('ROTA ' + req.originalUrl + ' foi acessada')
+}
